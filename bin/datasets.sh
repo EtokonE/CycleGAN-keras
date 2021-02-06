45aabfdbb6b667342a0e53d1482a66b84e57599f
Maxim Kalinin <maksim.kalinin.2110@gmail.com>
2021-02-07 02:41:15 +0300

    add load_model

--- bin/datasets.sh
+++ bin/datasets.sh
@@ -3,10 +3,10 @@ FILE=$1
 
 
 if [[ $FILE == 'monet2photo' ]]; then
-	URL=https://www.dropbox.com/sh/u4c5o3y2qwtipy5/AADtxxa0rcQvulDwuGOAgBHHa?dl=0
+	URL=https://www.dropbox.com/sh/nm7uttg933a586y/AABgesHRCJ9ueUCAHrHVrjFTa?dl=0
 	echo $URL
 elif [[ $FILE == 'portrait2photo' ]]; then
-	URL=https://www.dropbox.com/sh/r738501dvqnusbk/AACKessreIZhf3i3vXs3P1_wa?dl=0
+	URL=https://www.dropbox.com/sh/a900c04psizk86i/AAB3I1L6tT3_lscoxIeL2C5Va?dl=0
 	echo $URL
 else
 	echo "Available datasets are: monet2photo and portrait2photo"
--- bin/load_model.sh
+++ bin/load_model.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 MODEL=generatorB2A.h5
-URL=
-ZIP_FILE=../models/$MODEL.zip
-TARGET_DIR=../models/$MODEL/
-BOT_TARGET_DIR=../tg_bot/static/model/$MODEL/
+URL=https://www.dropbox.com/s/sg4pj0dvaydp6ey/generatorB2A.h5?dl=0
+TARGET_DIR=../models/$MODEL
+BOT_TARGET_DIR=../tg_bot/static/model/$MODEL
 curl -L $URL -o $ZIP_FILE
-mkdir $TARGET_DIR
-unzip $ZIP_FILE -d $TARGET_DIR
-mkdir $BOT_TARGET_DIR
-unzip $ZIP_FILE -d $BOT_TARGET_DIR
-rm $ZIP_FILE
+cp $TARGET_DIR $BOT_TARGET_DIR
